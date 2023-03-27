dd3f4d5ca927d8d31a76b3af65b6615d8275506e
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-03-27 07:05:02 -0500

    docs: :memo: Ejercicio 4
    Desarrollo Ejercicio 4 - Taller 2 - JavaScript
    
    by: Jhon Hernandez

--- Taller 1 js/main.js
+++ Taller 1 js/main.js
@@ -1,12 +1,26 @@
-/* 3. Construir el algoritmo para determinar el voltaje de un
-circuito 
+/* 4. Construir el algoritmo que solicite el nombre y edad de 3
+personas y determine el nombre de la persona con mayor edad. */
 
-resistencia  
-intensidad de corriente. */
+let persona1 = '';
+let edad1 = '';
+let persona2 = '';
+let edad2 = '';
+let persona3 = '';
+let edad3 = '';
 
-let corriente = prompt(`Digite la intensidad de corriente del circuito: `);
-let resistencia = prompt(`Digite la resistencia del circuito: `);
+persona1 = prompt('Digite el nombre de la primera persona: ');
+edad1 = prompt(`Digite la edad de ${persona1}`);
+persona2 = prompt('Digite el nombre de la segunda persona: ');
+edad2 = prompt(`Digite la edad de ${persona2}`);
+persona3 = prompt('Digite el nombre de la tercera persona: ');
+edad3 = prompt(`Digite la edad de ${persona3}`);
 
-let resultado = parseInt(corriente) * parseInt(resistencia);
+if(edad1 > edad2){
+    alert(`${persona1} es mayor con una edad de: ${edad1} años`);
 
-    alert(`El voltaje del circuito es de: ${resultado} voltios`);
+    } else if(edad2 > edad3){
+        alert(`${persona2} es mayor con una edad de: ${edad2} años`);
+
+    } else{
+        alert(`${persona3} es mayor con una edad de: ${edad3} años`);
+}
