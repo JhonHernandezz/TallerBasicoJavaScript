9c0f717dc50335abdf444a6a4263b5189fe18d8a
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-03-27 07:00:22 -0500

    docs: :memo: Ejercicio 2
    Desarrollo Ejercicio 2 - Taller 2 - JavaScript
    
    by: Jhon Hernandez

--- Taller 1 js/main.js
+++ Taller 1 js/main.js
@@ -1,23 +1,26 @@
-/* 1. Construir el algoritmo para un programa que ingrese tres notas de un alumno, 
-si el promedio es menor o igual a 3.9 mostrar un mensaje "Estudie“, 
-de lo contrario un mensaje que diga "becado" */
+/* 2. Dado un número indicar si es par o impar y si es mayor de 10. 
 
-let nota1 = prompt("Digite la nota #1: ");
+let numero = prompt("Digite un número: "); */
 
-if(nota1 >= 0 && nota1 <=10){
 
-    let nota2 = prompt("Digite la nota #2: ");
-    let nota3 = prompt("Digite la nota #3: ");
+if(numero <10){
 
-    let resultado = (parseInt(nota1) + parseInt(nota2) + parseInt(nota3)) / 3;
-
-    if(resultado >=0 && resultado <= 3.9){
-        alert(`Su nota es de: ${resultado.toFixed(1)} por favor "Estudie" más`)
-
+    if(numero % 2 == 0){
+        console.log(`El numero ${numero} es par`)
+       
     } else{
-        alert(`Su nota es de: ${resultado.toFixed(1)} esta "Becado" por esta institución`)
+        console.log(`El numero ${numero} es impar`)
     } 
 
 } else{
-    alert("Ingrese una calificación correcta");
+
+    console.log(`El numero ${numero} es mayor`)
+
+    if(numero % 2 == 0){
+        console.log(`Y es par`)
+       
+    } else{
+        console.log(`Y es impar`)
+    } 
+
 }
