faead2797270be304d1faa9f339d74e67e9cc181
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-03-27 13:28:13 -0500

    feat: :rocket: Metodos String  - Metodo charAt - Temas JavaScript
    Explicacion Metodo charAt - Temas JavaScript
    
    by: Jhon Hernandez

---
+++ Temas Js/main.js
@@ -0,0 +1,17 @@
+
+
+
+
+
+/* Metodo charAt */
+
+// devuelve la posición del carácter que le pasemos como parámetro en el interior de la cadena.
+
+// Si la cadena contiene varias ocurrencias del carácter que le pasemos como parámetro, 
+// el método charAt devolverá solamente la posición de la primera de ellas. 
+// Si el carácter no está presente en la cadena, el método charAt devolverá una cadena vacía.
+
+let cadena = 'JavaScript';
+let caracter = cadena.charAt(5); // 'J'
+
+console.log(caracter)
