39c22c6164cdb26019e9c8d7b8ee2f59601a249a
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-03-27 07:12:38 -0500

    docs: :memo: Ejercicio 7
    Desarrollo Ejercicio 7 - Taller 1 - JavaScript
    
    by: Jhon Hernandez

--- Taller 1 js/main.js
+++ Taller 1 js/main.js
@@ -1,54 +1,16 @@
-/* 6. Construir el algoritmo en Javascript con n cantidad de estudiantes
-que lea el nombre, el sexo y la nota definitiva 
+/* 7. Programa que pida el ingreso del nombre y precio de un artículo y la
+cantidad que lleva el cliente. Mostrar lo que debe pagar el comprador
+en su factura. */
 
-halle al estudiante con la mayor nota 
-y al estudiante con la menor nota 
-cuantos eran hombres y cuantos mujeres. */
+let nombre = [];
+let cantidad = [];
+let precio = [];
+let total = [];
 
-let nota = '';
-let estudiante = '';
-let sexo = '';
+nombre = prompt(`Digite el nombre del articulo a llevar:`);
+cantidad = prompt(`Digite la cantidad del articulo a llevar:`);
+precio = prompt(`Digite el precio del articulo a llevar:`);
 
-let masculino = 0;
-let femenimo = 0;
+total = parseInt(cantidad) * parseInt(precio);
 
-let calificacionMayor = 0;
-let calificacionMenor = 100;
-
-let nombreCalificacionMayor = "";
-let nombreCalificacionMenor = "";
-
-let cantidad = prompt("Digite la cantidad de estudiantes a evaluar: ");
-
-for (i=0; i<cantidad; i++){
-    estudiante = prompt(`Digite el nombre del estudiante #${i +1}`);
-    sexo = prompt(`Digite el sexo del estudiante "M" o "F" #${i +1}`);
-    nota = parseFloat(prompt(`Digite la nota del estudiante de 1 - 100 #${i +1}`));
-
-    if( nota >= 0 && nota <= 100){
-        if (nota > calificacionMayor) {
-            calificacionMayor = nota;
-            nombreCalificacionMayor = estudiante;
-        }
-    
-        if (nota < calificacionMenor) {
-            calificacionMenor = nota;
-            nombreCalificacionMenor = estudiante;
-        }
-
-    } else{
-        alert("Digite una nota correcta!");
-        break;
-    }
-
-    if (sexo === "M" ){
-        masculino++;
-    } else if (sexo === "F" ){
-        femenimo++;
-    }
-
-}
-
-console.log(`${nombreCalificacionMayor} obtuvo una nota de: ${calificacionMayor} y fue el estudiante con el mejor desempeño`);
-console.log(`${nombreCalificacionMenor} obtuvo una nota de: ${calificacionMenor} y fue el estudiante con el peor desempeño`);
-console.log(`En la clase hay un total de: ${masculino} hombres y ${femenimo} mujeres`);
+console.table(`Nombre: ${nombre} \nCantidad: ${cantidad} \nPrecio unidad: ${precio} \nTotal: ${total}`);
