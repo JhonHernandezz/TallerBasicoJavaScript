6848c06efc103f0ddf2ea01d15c686fc7452c277
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 12:50:15 -0500

    docs: :memo: /* Operadores aritmeticos */ /* Suma */
    Explicacion Operadores aritmeticos suma - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,10 +1,10 @@
-/* Metodo trim */
+/* Operadores aritmeticos */
+     
+        /* suma */
 
-// elimina los espacios en blanco en ambos extremos del string. Los espacios en blanco en este contexto, son todos los caracteres sin contenido (espacio, tabulación, etc.) y todos los caracteres de nuevas lineas (LF,CR,etc.).
+let suma1 = 5;
+let suma2 = 6;
 
-let orig = '   foo  ';
-console.log(orig.trim()); // 'foo'
+let resultado = suma1 + suma2 ;
 
-// Otro ejemplo de .trim() eliminando el espacio en blanco sólo de un lado.
-orig = 'foo    ';
-console.log(orig.trim()); // 'foo'
+console.log(resultado)
