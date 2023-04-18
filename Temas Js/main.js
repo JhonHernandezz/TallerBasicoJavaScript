b114fd532cc24bd1c7c126747e10913ae9642855
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 12:35:40 -0500

    docs: :memo: /* Metodo replace */
    Explicacion Metodo replace - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,11 +1,11 @@
-/* Metodo repeat */
+/* Metodo replace */
 
-// devuelve una nueva cadena que contiene el número especificado de copias de la cadena en la cual fue llamada, concatenados.
+// devuelve una nueva cadena con algunas o todas las coincidencias de un patrón, 
+// siendo cada una de estas coincidencias reemplazadas por remplazo. 
+// reemplaza el 2do valor que se le de 
 
-let repetir1 = 'abc'.repeat(1);    // 'abc'
-let repetir2 = 'abc'.repeat(2);    // 'abcabc'
-let repetir3 = 'abc'.repeat(3.5);  // 'abcabcabc' (count will be converted to integer)
+let repetir1 = 'The quick brown fox jumps over the lazy dog. If the dog reacted, was it really lazy?';
 
 console.log(repetir1);
-console.log(repetir2);
-console.log(repetir3);
+
+console.log(repetir1.replace('dog', 'MONKEY'));
