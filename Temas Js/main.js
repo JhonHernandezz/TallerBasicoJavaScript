e85881adac3371f1b3b88c4cb5e366647b1d2c4c
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 12:45:13 -0500

    docs: :memo: /* Metodo trim */
    Explicacion Metodo trim - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,6 +1,10 @@
-/* Metodo toUpperCase */
+/* Metodo trim */
 
-// método devuelve el valor convertido en mayúsculas de la cadena que realiza la llamada.
+// elimina los espacios en blanco en ambos extremos del string. Los espacios en blanco en este contexto, son todos los caracteres sin contenido (espacio, tabulación, etc.) y todos los caracteres de nuevas lineas (LF,CR,etc.).
 
-let textoMayusculas="alfabeto"
-console.log(textoMayusculas.toLocaleUpperCase())
+let orig = '   foo  ';
+console.log(orig.trim()); // 'foo'
+
+// Otro ejemplo de .trim() eliminando el espacio en blanco sólo de un lado.
+orig = 'foo    ';
+console.log(orig.trim()); // 'foo'
