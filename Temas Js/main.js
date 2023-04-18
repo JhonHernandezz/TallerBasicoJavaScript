17ff5398ff8087f8d0793b3ad43e072203269065
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 13:20:24 -0500

    docs: :memo: /* OR || */
    Explicacion Operadores de comparacion /* OR || */  - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,10 +1,10 @@
-    /* AND && */
+    /* OR || */
 
 let numero1 = 5;
 let numero2 = 6;
 
-if(numero1 >= 1  && numero2 <= 10) { 
-    console.log(`Verdadero`); // Si numero 1 es mayor o igual 1 y numero 2 es menor o igual a 10
+if(numero1 >= 1  || numero2 <= 10) { 
+    console.log(`Verdadero`); // Si numero 1 es mayor o igual 1 o numero 2 es menor o igual a 10
 } else{
-    console.log(`Falso`) // Si numero 1 no es mayor ni igual a 1 y numero 2 no es menor o igual 10
+    console.log(`Falso`) // Si numero 1 no es mayor ni igual a 1 o numero 2 no es menor o igual 10
 }
