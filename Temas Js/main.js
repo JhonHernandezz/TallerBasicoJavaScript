2c53cca4622e87d5a253041d68fef4cb33656d9c
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 13:15:20 -0500

    docs: :memo: /* Mayor o igual que >= */
    Explicacion Operadores de comparacion /* Mayor o igual que >= */  - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,10 +1,10 @@
-    /* Mayor > */
+    /* Mayor o igual que >= */
 
 let numero1 = 5;
 let numero2 = 6;
 
-if(numero1 > numero2) {
-    console.log("Verdadero");
+if(numero1 >= numero2) {
+    console.log(`El numero ${numero1} es mayor pero no es igual que ${numero2}`);
 } else{
-    console.log("Falso")
+    console.log(`El numero ${numero1} no es mayor ni igual que ${numero2}`)
 }
