8a8d413ad8a825e2486d53f37a9b5644b33c3408
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 13:07:22 -0500

    docs: :memo: /* no es igual !=  */
    Explicacion Operadores de comparacion /* no es igual !=  */ - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,11 +1,9 @@
-/* Operadores de comparacion */
-
-            /* igual == */
+    /* no es igual != */
 
 let numero1 = 5;
 let numero2 = 6;
 
-if(numero1 == numero2) {
+if(numero1 != numero2) {
     console.log("Verdadero");
 } else{
     console.log("Falso")
