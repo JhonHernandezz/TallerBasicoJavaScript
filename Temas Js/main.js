f1511e0b0af71ceec9adb816e7ca9cff9d4ba897
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 13:28:38 -0500

    docs: :memo: /* Metodo random */
    Explicacion libreria matematica Metodo random - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,7 +1,5 @@
-/* Metodo pow */
+/* Metodo random */
 
-// devuelve la base elevada al exponente , esto es, baseexponente
+// Devuelve un numero ramdom
 
-console.log(Math.pow(7, 2));    // 49
-console.log(Math.pow(7, 3));    // 343
-console.log(Math.pow(2, 3));   // 1024
+console.log(Math.random().toFixed(3));
