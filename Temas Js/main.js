49633cc4fea58142f7e3d5f80170ed7e787b7fc5
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 13:17:42 -0500

    docs: :memo: /* Menor o igual que <= */
    Explicacion Operadores de comparacion /* Menor o igual que <= */  - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,10 +1,10 @@
-    /* Menor < */
+    /* Menor o igual que <= */
 
 let numero1 = 5;
 let numero2 = 6;
 
-if(numero1 < numero2) {
-    console.log("Verdadero");
+if(numero1 <= numero2) {
+    console.log(`El numero ${numero1} es menor pero no es igual que ${numero2}`);
 } else{
-    console.log("Falso")
+    console.log(`El numero ${numero1} no es menor ni igual que ${numero2}`)
 }
