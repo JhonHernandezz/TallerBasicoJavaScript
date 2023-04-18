107c657780a2186c500690148d28a18999d4efc4
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 13:09:11 -0500

    docs: :memo: /* estrictamente igual === */
    Explicacion Operadores de comparacion /* estrictamente igual === */  - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,9 +1,9 @@
-    /* no es igual != */
+    /* estrictamente igual === */
 
 let numero1 = 5;
 let numero2 = 6;
 
-if(numero1 != numero2) {
+if(numero1 === numero2) {
     console.log("Verdadero");
 } else{
     console.log("Falso")
