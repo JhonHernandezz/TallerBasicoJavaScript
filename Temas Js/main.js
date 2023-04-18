c28c66b17dc8d842ae319735b783ddfa5177439b
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 12:34:21 -0500

    docs: :memo: /* Metodo repeat */
    Explicacion Metodo repeat - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,15 +1,11 @@
-/* Metodo matchAll */
+/* Metodo repeat */
 
-// retorna un iterador de todos los resultados de ocurrencia en una cadena de texto contra una expresión regular, 
-// incluyendo grupos de captura.
+// devuelve una nueva cadena que contiene el número especificado de copias de la cadena en la cual fue llamada, concatenados.
 
-const regexp = /t(e)(st(\d?))/g;
-const str = 'test1test2';
+let repetir1 = 'abc'.repeat(1);    // 'abc'
+let repetir2 = 'abc'.repeat(2);    // 'abcabc'
+let repetir3 = 'abc'.repeat(3.5);  // 'abcabcabc' (count will be converted to integer)
 
-const array = [...str.matchAll(regexp)];
-
-console.log(array[0]);
-// Expected output: Array ["test1", "e", "st1", "1"]
-
-console.log(array[1]);
-// Expected output: Array ["test2", "e", "st2", "2"]
+console.log(repetir1);
+console.log(repetir2);
+console.log(repetir3);
