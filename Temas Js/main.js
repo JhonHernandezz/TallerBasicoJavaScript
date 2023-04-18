b9faed145e11efc1662051893aafb23a2b203ed0
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-18 12:23:21 -0500

    docs: :memo: /* Metodo codePointAt */
    Explicacion Metodo codePointAt - Temas JavaScript
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,15 +1,11 @@
-/* Metodo charCodeAt */
+/* Metodo codePointAt */
 
-// devuelve el codigo Unicode de los caracteres que esten en la posición que le pasemos.
-// El método CharCodeAt es aceptado por las cadenas en JavaScript, 
-// devolviendo el código Unicode de 16 bits del carácter que esté en la posición que le pasemos como parámetro al método.
+// devuelve un entero no negativo que equivale al valor Unicode code point del carácter.
 
-let codigo1 = 'JavaScript'.charCodeAt(0) // 74
-let codigo2 = 'JavaScript'.charCodeAt(1) // 97
-let codigo3 = 'JavaScript'.charCodeAt(2) // 118
-let codigo4 = 'JavaScript'.charCodeAt(3) // 97
+let codigo1 = 'ABC'.codePointAt(1);          // 66
+let codigo2 = '\uD800\uDC00'.codePointAt(0); // 65536
+let codigo3 = 'XYZ'.codePointAt(52); // undefined
 
 console.log(codigo1)
 console.log(codigo2)
 console.log(codigo3)
-console.log(codigo4)
