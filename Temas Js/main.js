aed8b6e6bc8a0a94eb58d2d122a53231e9f3b4d5
Jhon Hernandez <jhonhernandez.campus@gmail.com>
2023-04-20 06:59:40 -0500

    feat: :rocket: /* Funciones Callback */
    Explicación y ejemplos de las funciones Callback
    
    by: Jhon Hernandez

--- Temas Js/main.js
+++ Temas Js/main.js
@@ -1,5 +1,32 @@
-/* Metodo trunc */
+/* Funciones Callback */
 
-// devuelve la parte entera de un numero removiendo cualquier dígito decimal (dígitos situados después de la coma).
+// Una función de callback es una función que se pasa a otra función como un argumento
+// que luego se invoca dentro de la función externa para completar algún tipo de rutina o acción.
 
-console.log(Math.trunc(12.8));
+// JavaScript ejecuta el código secuencialmente en orden descendente. 
+// Sin embargo, hay algunos casos en los que el código se ejecuta (o debe ejecutarse) después de que ocurra otra cosa y también de forma no secuencial. 
+// Esto se llama programación asíncrona.
+
+listaToque = [];
+
+let boton = document.querySelector("#callback-btn");
+
+boton.addEventListener("click", function() {  
+    alert("El usuario ha hecho clic en el botón.");
+});
+
+/* function comer(hora, comida) {
+    comida (hora);  
+}
+    
+function desayuno(hora) {   
+    alert(`${hora}: Desayuno`);   
+}
+    
+function desayuno2(hora) {   
+    alert(`${hora}: Segundo Desayuno`);   
+} */
+
+/* setTimeout(() => { 
+    alert("Este mensaje se muestra después de 3 segundos");
+}, 3000); */
